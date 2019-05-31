2242e8f213321dd1ff5f0a9f511046a18c549cdf
SoLoDas <rowankh@gmail.com>
2019-05-31 16:47:38 +0100

    removed updator code

--- update.js
+++ update.js
@@ -1,15 +1,16 @@
-var request = require('request');
-const fs = require('fs');
-request('http://98.7.203.224:42125/getLatestVersion', function (error, response, body) {
-  if(body != fs.readFileSync("./version.txt", 'utf-8')) {
-    console.log(body, fs.readFileSync("./version.txt", 'utf-8'))
-    var pipe = request('http://98.7.203.224:42125/getUpdateZip').pipe(fs.createWriteStream("./update.zip"))
-    pipe.on('finish', () => {
-      console.log("UPDATE FINISHED")
+
+  var request = require('request');
+  const fs = require('fs');
+  request('http://98.7.203.224:42125/getLatestVersion', function (error, response, body) {
+    if(body != fs.readFileSync("./version.txt", 'utf-8')) {
+      console.log(body, fs.readFileSync("./version.txt", 'utf-8'))
+      var pipe = request('http://98.7.203.224:42125/getUpdateZip').pipe(fs.createWriteStream("./update.zip"))
+      pipe.on('finish', () => {
+        console.log("UPDATE FINISHED")
+        require('./index.js')
+      })
+    } else {
+      console.log("NO UPDATE NEEDED")
       require('./index.js')
-    })
-  } else {
-    console.log("NO UPDATE NEEDED")
-    require('./index.js')
-  }
-});
+    }
+  });
