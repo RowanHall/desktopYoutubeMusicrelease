bed7a6061236a3f06a917cb8ca2f49067253a435
RowanHall <rowankh@gmail.com>
2019-05-31 10:46:47 -0400

    added email check

--- musicShareHTML.js
+++ musicShareHTML.js
@@ -6,27 +6,29 @@ class MyEmitter extends EventEmitter {}
 window.myEmitter = new MyEmitter();
 
 myEmitter.on('NEW_USER' , function(data){
-  var icons = [`
-    <img class="svgKick" src="./outline-highlight_off-24px.svg" onclick="kick()">`,`
-    <img class="svgOwner" src="./outline-verified_user-24px.svg">`]
-  var icon = ``;
-  if(data.owner) {
-    icon = icons[1]
-  } else {
-    icon = icons[0]
-  }
-  var html = `<div class="connectedUser">
-    <div class="childLeft">
-      <img src="${data.icon}" height="55px"></img>
-    </div>
-    <div class="childRight">
-      <div class="childinnerRight">${icon}
+  if(data.email) {
+    var icons = [`
+      <img class="svgKick" src="./outline-highlight_off-24px.svg" onclick="kick()">`,`
+      <img class="svgOwner" src="./outline-verified_user-24px.svg">`]
+    var icon = ``;
+    if(data.owner) {
+      icon = icons[1]
+    } else {
+      icon = icons[0]
+    }
+    var html = `<div class="connectedUser">
+      <div class="childLeft">
+        <img src="${data.icon}" height="55px"></img>
       </div>
-      <pre class="user">${data.name}</pre>
-      <pre class="email">${data.email}</pre>
-    </div>
-  </div>`
-  document.getElementById('midDiv').innerHTML = document.getElementById('midDiv').innerHTML + html;
+      <div class="childRight">
+        <div class="childinnerRight">${icon}
+        </div>
+        <pre class="user">${data.name}</pre>
+        <pre class="email">${data.email}</pre>
+      </div>
+    </div>`
+    document.getElementById('midDiv').innerHTML = document.getElementById('midDiv').innerHTML + html;
+  }
 });
 
 myEmitter.on('REMOVE_USER' , function(data){
