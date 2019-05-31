1516ec52e4c414d8b575928793fe34701fd755c0
SoLoDas <rowankh@gmail.com>
2019-05-31 16:45:21 +0100

    added debug info

--- update.js
+++ update.js
@@ -6,6 +6,10 @@ request('http://98.7.203.224:42125/getLatestVersion', function (error, response,
     var pipe = request('http://98.7.203.224:42125/getUpdateZip').pipe(fs.createWriteStream("./update.zip"))
     pipe.on('finish', () => {
       console.log("UPDATE FINISHED")
+      require('./index.js')
     })
+  } else {
+    console.log("NO UPDATE NEEDED")
+    require('./index.js')
   }
 });
