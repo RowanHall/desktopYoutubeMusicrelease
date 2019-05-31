400b6fde61922f9ae492717b121b0a8dfed5a7b2
SoLoDas <rowankh@gmail.com>
2019-05-31 16:51:34 +0100

    compilefix

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusic YouTube-Music --platform=darwin
-call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusic YouTube-Music --platform=linux
-call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusic YouTube-Music --platform=mas
-call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusic YouTube-Music --platform=win32
+call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusicgitrelease YouTube-Music --platform=darwin
+call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusicgitrelease YouTube-Music --platform=linux
+call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusicgitrelease YouTube-Music --platform=mas
+call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusicgitrelease YouTube-Music --platform=win32
--- compilewindows.bat
+++ compilewindows.bat
@@ -1,2 +1,2 @@
 echo "ONLY COMPILING FOR PLATFORM win32, PLEASE USE compile FOR THE FINAL EXPORT"
-call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusic YouTube-Music --platform=win32
+call electron-packager C:\Users\rowan\Documents\GitHub\desktopYoutubeMusicgitrelease YouTube-Music --platform=win32
